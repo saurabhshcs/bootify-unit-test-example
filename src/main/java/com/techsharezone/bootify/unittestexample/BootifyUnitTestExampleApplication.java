60b02774c64f58d630ab1ecb401ad18dcad7b782
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-02-13 23:25:08 +0000

    initial-project-structre-committed

---
+++ src/main/java/com/techsharezone/bootify/unittestexample/BootifyUnitTestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootify.unittestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifyUnitTestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifyUnitTestExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsharezone/bootify/unittestexample/BootifyUnitTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootify.unittestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifyUnitTestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
